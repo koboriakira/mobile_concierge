2b6692bf811be4fcaa688a7fed6b5b4607d8f66e
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-06-21 14:15:25 +0900

    refactor: Update completeTask method to clear in-progress task and current tasks

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,8 @@ class _MyAppState extends State<MyApp> {
   /// 仕掛中タスクを完了します。
   Future<void> completeTask() async {
     setState(() {
-      isApiExecuting = true;
+      inprogressTask = null;
+      currentTasks = [];
     });
     var _ = await _taskRepository.completeTask(inprogressTask!.pageId);
     upToDate();
